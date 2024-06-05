9d0f30ebe12ce9569d0487c68d0b638fab5f9ac0
Deepti rai <144608021+deepti2207@users.noreply.github.com>
2024-06-05 11:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-// your code here
+	if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    }
+
+    // Initialize the first two Fibonacci numbers
+    let a = 0, b = 1;
+
+    // Compute Fibonacci numbers using iterative approach
+    for (let i = 2; i <= num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
+
 }
 
 module.exports = fibonacci;
